981978f32b3091e5b82212d603af2ab799686ef8
debankanmitra <debankanmitra@gmail.com>
2024-03-21 11:08:33 +0530

    candlestick pattern recognition

---
+++ candlestick_pattern/request.py
@@ -0,0 +1,43 @@
+import datetime
+import os
+import requests
+import pandas as pd
+from candlestick_pattern import patterns
+
+
+def convert_to_datetime(timestamp):
+  # Convert milliseconds to seconds
+  timestamp_seconds = timestamp / 1000
+
+  # Convert Unix timestamp to datetime
+  dt_object = datetime.datetime.fromtimestamp(timestamp_seconds)
+
+  # Print the datetime object
+  return dt_object
+
+def change_DataFrame(data):
+    df = pd.DataFrame(data)
+    df.columns = ["timestamp","Open", "High", "Low", "Close"]
+    df['Pattern']=None
+
+    # Apply the function to the timestamp column
+    df['timestamp'] = df['timestamp'].apply(convert_to_datetime)
+
+    return df
+
+def get_data(ticker, days, currency):
+    url = f"https://api.coingecko.com/api/v3/coins/{ticker}/ohlc?vs_currency={currency}&days={days}&precision=0"
+
+    token = os.environ.get('COINGECKO_TOKEN')
+
+    headers = {
+    "Authorization": f"Bearer {token}",
+    "Content-Type": "application/json",
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    new_data = change_DataFrame(data)
+    new_data_with_pattern=patterns.recognized_pattern(new_data)
+    
+    return new_data_with_pattern
---
+++ main.py
@@ -0,0 +1,3 @@
+from candlestick_pattern.request import get_data
+
+print(get_data('bitcoin', 1, 'usd'))
